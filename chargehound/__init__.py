4b3c02d5964857748b5e3e0c6faeb1b732b57332
jj <jj.weber@gmail.com>
2016-07-06 10:33:44 -0700

    Timeout after 5 seconds

--- chargehound/__init__.py
+++ chargehound/__init__.py
@@ -1,6 +1,5 @@
 from chargehound.resources import Disputes
 
-
 api_key = None
 host = 'api.chargehound.com'
 base_path = '/v1/'
--- chargehound/api_requestor.py
+++ chargehound/api_requestor.py
@@ -43,13 +43,15 @@ class APIRequestor(object):
                                        auth=auth,
                                        params=params,
                                        headers=headers,
-                                       hooks=hooks))
+                                       hooks=hooks,
+                                       timeout=5))
         elif method == 'post':
             return self.parse_response(requests.post(self.get_url(path),
                                        auth=auth,
                                        json=data,
                                        headers=headers,
-                                       hooks=hooks))
+                                       hooks=hooks,
+                                       timeout=5))
 
     def request(self, method, path, params=None, data=None, callback=None):
         if callback is None:
