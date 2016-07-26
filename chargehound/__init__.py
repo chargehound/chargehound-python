261421a647fae9eb4df998c26740f7141a68c13d
jj <jj.weber@gmail.com>
2016-07-26 10:39:53 -0700

    Set timeout to 60 seconds

--- chargehound/__init__.py
+++ chargehound/__init__.py
@@ -3,6 +3,6 @@ from chargehound.resources import Disputes
 api_key = None
 host = 'api.chargehound.com'
 base_path = '/v1/'
-timeout = 5
+timeout = 60
 
 __all__ = [api_key, host, Disputes, timeout]
