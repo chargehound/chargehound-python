1bc7a5bccea7e1e420436a8bcb2343172aa4d24c
jj <jj.weber@gmail.com>
2017-06-01 16:25:24 -0700

    Release info

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ a new version.""")
 
 setup(
     name='chargehound',
-    version='2.1.1',
+    version='2.1.2',
     author='Chargehound',
     author_email='support@chargehound.com',
     packages=['chargehound'],
